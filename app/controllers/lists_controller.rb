6bc0c713e9e61914f4c56880f4f44757779c12a5
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-04-16 10:59:27 +0000

    [Update] 3-2 フォームから投稿する

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,12 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
   def index
   end
 
@@ -11,4 +17,10 @@ class ListsController < ApplicationController
 
   def edit
   end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
 end
